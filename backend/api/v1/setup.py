c2a4a45b2678282f999c70d798bb84707026b84f
Marcelino Coll Rovira <coll.marce@gmail.com>
2016-08-05 11:36:11 -0700

    Added preliminary setup.py

---
+++ backend/api/v1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setuptools
+
+setup(name='hackupcapi',
+	  version='0.1',
+	  description='HackUPC API v1',
+	  url='https://github.com/hackupc/hackupc-2016-API',
+	  author='hackupc',
+	  install_requires=[
+	  	'sqlalchemy',
+	  	'flask'
+	  ]
+	)
