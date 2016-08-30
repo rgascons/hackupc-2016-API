89610f8e3c094c5e929fcfa1d616eef185a1c8cf
debenoprma <coll.marce@gmail.com>
2016-08-30 12:00:33 -0700

    Added installation script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Install virtualenv if we don't have it
+pip install virtualenv
+
+# Create python virtual environment and activate it
+VENVDIR="$DIR/venv"
+mkdir "$VENVDIR"
+virtualenv -p python2.7 "$VENVDIR"
+. "$VENVDIR/bin/activate"
+
+# Upgrade pip
+php install --upgrade pip
+
+# Install requirements.txt
+pip install -r "$DIR/backend/requirements.txt"
+
+# Create settings.py from the example
+echo "Typeform API key:"
+read typeformKey
+
+echo "Typeform FormID:"
+read typeformID
+
+cp "$DIR/backend/api/v1/settings.example.py" "$DIR/backend/api/v1/settings.py"
+sed -i "s/typeformapikey/$typeformKey/g" "$DIR/backend/api/v1/settings.py"
+sed -i "s/typeformformid/$typeformID/g" "$DIR/backend/api/v1/settings.py"
+
+echo "Application Manager is installed, now add it to nginx"
